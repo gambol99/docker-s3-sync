7d262dd7361cfb1309bc9415aed5ec6a9e39d987
Rohith <gambol99@gmail.com>
2016-02-03 11:34:40 +0000

    - adding the ability to sync individual files as well

--- run.sh
+++ run.sh
@@ -8,12 +8,14 @@
 
 OUTPUT_DIR=${OUTPUT_DIR:-""}
 INTERVAL=${INTERVAL:="10"}
+SYNC_FILES=${SYNC_FILES:-""}
 
 usage() {
   cat <<EOF
   Usage: $(basename $0) [options]
     --output-dir|-o  PATH      : the directory to write the files to (OUTPUT_DIR)
     --bucket|-b      NAME      : the aws bucket in s3://NAME/KEY format (AWS_BUCKET)
+    --files|-f       FILES     : synchronize a list of files, not the entire buckut (SYNC_FILES)
     --interval|-I    SECONDS   : the interval in seconds between syncing the files (INTERVAL)
     --help|-h                  : display this usage menu
 EOF
@@ -27,6 +29,7 @@ EOF
 while [ $# -ne 0 ]; do
   case "$1" in
     --output-dir|-o)    OUTPUT_DIR=$2; shift 2 ;;
+    --files|-f)         SYNC_FILES="$SYNC_FILES $2"; shift ;;
     --bucket|-b)        AWS_BUCKET=$2; shift 2 ;;
     --interval|-i)      INTERVAL=$2;   shift 2 ;;
     --help|-h)          usage; ;;
@@ -40,6 +43,12 @@ echo "synchronization from: $AWS_BUCKET, dest: ${OUTPUT_DIR} internal: ${INTERVA
 [ -z "${OUTPUT_DIR}" ] && usage "you have not specified a output directory"
 
 while true; do
-  aws s3 sync --only-show-errors ${AWS_BUCKET} ${OUTPUT_DIR}
+  if [ -z "$SYNC_FILES" ]; then
+    echo aws s3 sync --only-show-errors ${AWS_BUCKET} ${OUTPUT_DIR}
+  else
+    for _filename in $SYNC_FILES; do
+      echo aws s3 cp --only-show-errors ${AWS_BUCKET}/${_filename} ${OUTPUT_DIR}
+    done
+  fi
   sleep $INTERVAL
 done
